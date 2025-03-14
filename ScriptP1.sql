6425e5d55d017d2ed59e1dd2353926e588d73d45
Sama <3664015720115@ingenieria.usac.edu.gt>
2025-03-14 01:10:19 -0600

    Script
    Tabla sin referencias faltante

--- ScriptP1.sql
+++ ScriptP1.sql
@@ -1,3 +1,12 @@
+--Deshabilita las claves foraneas en caso de que existan en alguna tabla y no hagan conflicto
+--Elmina llave foranea CLIENTE
+ALTER TABLE CLIENTE DROP CONSTRAINT fk_info_cliente;
+
+ALTER TABLE PRODUCTO DROP CONSTRAINT fk_categoria;
+ALTER TABLE PRODUCTO DROP CONSTRAINT fk_sku;
+ALTER TABLE PRODUCTO DROP CONSTRAINT fk_slug;
+
+
 DROP TABLE test_table;
 CREATE TABLE test_table (
     id NUMBER PRIMARY KEY,
@@ -27,6 +36,7 @@ CREATE TABLE Lugar_Entrega(
     updated_at TIMESTAMP
 );
 
+
 DROP TABLE SKU;
 CREATE TABLE SKU(
     id NUMBER PRIMARY KEY,
@@ -55,6 +65,16 @@ CREATE TABLE DEPARTAMENTO(
     updated_at TIMESTAMP
 );
 
+DROP TABLE Info_trabajador;
+CREATE TABLE Info_trabajador(
+    id NUMBER PRIMARY KEY,
+    correo VARCHAR2(50) NOT NULL,
+    numero VARCHAR2(20) NOT NULL,
+    puesto VARCHAR2(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+
 DROP TABLE SEDE;
 CREATE TABLE SEDE(
     id NUMBER PRIMARY KEY,
@@ -71,25 +91,58 @@ CREATE TABLE CATEGORIAS(
     updated_at TIMESTAMP
 );
 
+DROP TABLE CLIENTE;
+CREATE TABLE CLIENTE(
+    id NUMBER PRIMARY KEY,
+    Documento_nacional VARCHAR2(20) NOT NULL,
+    Nombre_Cliente VARCHAR2(50) NOT NULL,
+    Apellido_Cliente VARCHAR2(50) NOT NULL,
+    Actiivo VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    id_info_cliente NUMBER
+);
+ALTER TABLE CLIENTE ADD CONSTRAINT fk_info_cliente FOREIGN KEY (id_info_cliente) REFERENCES Info_Cliente(id);
+
+DROP TABLE PRODUCTO;
+CREATE TABLE PRODUCTO(
+    id NUMBER PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    Id_Categoria NUMBER,
+    id_sku NUMBER,
+    id_slug NUMBER
+);
+ALTER TABLE PRODUCTO ADD CONSTRAINT fk_categoria FOREIGN KEY (Id_Categoria) REFERENCES CATEGORIAS(id);
+ALTER TABLE PRODUCTO ADD CONSTRAINT fk_sku FOREIGN KEY (id_sku) REFERENCES SKU(id);
+ALTER TABLE PRODUCTO ADD CONSTRAINT fk_slug FOREIGN KEY (id_slug) REFERENCES SLUG(id);
+
 
 INSERT INTO test_table (id, name) VALUES (1, 'Hola');
-INSERT INTO test_table (id, name) VALUES (3, 'Test Name 2');
+INSERT INTO test_table (id, name) VALUES (2, 'Test Name 2');
 --INFORMACION DE CLIENTES
 INSERT INTO Info_Cliente (id, correo, correo_confirmado, passkey, numero, created_at, updated_at) VALUES (1, 'juan.perez@example.com', 'TRUE', 'passkey123', '555-1234', SYSTIMESTAMP, NULL);
 INSERT INTO Info_Cliente (id, correo, correo_confirmado, passkey, numero, created_at, updated_at) VALUES (2, 'jose.perez@example.com', 'FALSE', 'MYPASSS', '*53 1252135556', SYSTIMESTAMP, NULL);
 --LUGARES DE ENTREGA
-INSERT INTO Lugar_Entrega (id, Numero_Hogar, Nombre_CA, Ciudad_Entrega, Estado_Entrega, Codigo_Postal, created_at, updated_at) VALUES (7, '123', 'Casa de Juan', 'Guadalajara', 'Jalisco', '44100', SYSTIMESTAMP, NULL);
+INSERT INTO Lugar_Entrega (id, Numero_Hogar, Nombre_CA, Ciudad_Entrega, Estado_Entrega, Codigo_Postal, created_at, updated_at) VALUES (1, '123', 'Casa de Juan', 'Guadalajara', 'Jalisco', '44100', SYSTIMESTAMP, NULL);
 --SKU DE PRODUCTOS
-INSERT INTO SKU (id, SKU, Nombre_Producto, Descripccion_Producto, Precio_Producto, created_at, updated_at) VALUES (9, 'SKU123', 'Producto 1', 'Descripccion del Producto 1', 100, SYSTIMESTAMP, NULL);
+INSERT INTO SKU (id, SKU, Nombre_Producto, Descripccion_Producto, Precio_Producto, created_at, updated_at) VALUES (1, 'SKU123', 'Producto 1', 'Descripccion del Producto 1', 100, SYSTIMESTAMP, NULL);
 --SLUGS DE PRODUCTOS
-INSERT INTO SLUG (id, SLUD_description, created_at, updated_at) VALUES (12, 'SLUG 1', SYSTIMESTAMP, NULL);
+INSERT INTO SLUG (id, SLUD_description, created_at, updated_at) VALUES (1, 'SLUG 1', SYSTIMESTAMP, NULL);
 --DEPARTAMENTOS
 INSERT INTO DEPARTAMENTO (id, Funcion_Departamento, Nombre_Departamento, created_at, updated_at) VALUES (1, 'Funcion 1', 'Departamento 1', SYSTIMESTAMP, NULL);
+--INFORMACION DE TRABAJADORES
+INSERT INTO Info_trabajador (id, correo, numero, puesto, created_at, updated_at) VALUES (1, 'ejemplo@correo.com', '1234567890', 'Desarrollador', SYSTIMESTAMP, NULL);
 --SEDES
-INSERT INTO SEDE (id, Nombre_Sede, created_at, updated_at) VALUES (3, 'Sede 1', SYSTIMESTAMP, NULL);
-INSERT INTO SEDE (id, Nombre_Sede, created_at, updated_at) VALUES (4, 'Sede 2', SYSTIMESTAMP, NULL);
+INSERT INTO SEDE (id, Nombre_Sede, created_at, updated_at) VALUES (1, 'Sede 1', SYSTIMESTAMP, NULL);
+INSERT INTO SEDE (id, Nombre_Sede, created_at, updated_at) VALUES (2, 'Sede 2', SYSTIMESTAMP, NULL);
 --CATEGORIAS
-INSERT INTO CATEGORIAS (id, Nombre_Categoria, created_at, updated_at) VALUES (14, 'Categoria 1', SYSTIMESTAMP, NULL);
+INSERT INTO CATEGORIAS (id, Nombre_Categoria, created_at, updated_at) VALUES (1, 'Categoria 1', SYSTIMESTAMP, NULL);
+--CLIENTES
+INSERT INTO CLIENTE (id, Documento_nacional, Nombre_Cliente, Apellido_Cliente, Actiivo, created_at, updated_at, id_info_cliente) VALUES (1, '123456', 'Juan', 'Perez', 'TRUE', SYSTIMESTAMP, NULL, 1);
+--PRODUCTOS
+INSERT INTO PRODUCTO (id, created_at, updated_at, Id_Categoria, id_sku, id_slug) VALUES (1, SYSTIMESTAMP, NULL, 1, 1, 1);
+
 
 COMMIT;
 
@@ -99,5 +152,8 @@ SELECT * FROM Lugar_Entrega;
 SELECT * FROM SKU;
 SELECT * FROM SLUG;
 SELECT * FROM DEPARTAMENTO;
+SELECT * FROM Info_trabajador;
 SELECT * FROM SEDE;
 SELECT * FROM CATEGORIAS;
+SELECT * FROM CLIENTE;
+SELECT * FROM PRODUCTO;
